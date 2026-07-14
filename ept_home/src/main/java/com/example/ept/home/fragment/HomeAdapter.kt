5721b55239f5fcaf299dbdb55e89327ad2985147
SCDOMM <a977628@outlook.com>
2026-07-14 22:28:08 +0800

    :sparkle:增加xml布局，搭建基础结构

--- ept_home/src/main/java/com/example/ept/home/fragment/HomeAdapter.kt
+++ ept_home/src/main/java/com/example/ept/home/fragment/HomeAdapter.kt
@@ -1,6 +1,12 @@
 package com.example.ept.home.fragment
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+
+import com.example.core.model.videoEntity.VideoData
 
 /**   
  * 包名称： com.example.ept.home.fragment
@@ -10,5 +16,42 @@ import androidx.recyclerview.widget.RecyclerView
  * 创建时间：2026-07-14 16:44
  *
  */
-class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>(){
+class HomeAdapter : ListAdapter<VideoData, HomeAdapter.ViewHolder>(HomeDiffCallback){
+    private val videoList: MutableList<VideoData> = mutableListOf()
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+
+    }
+
+
+
+
+}
+object HomeDiffCallback: DiffUtil.ItemCallback<VideoData>(){
+    override fun areItemsTheSame(
+        oldItem: VideoData,
+        newItem: VideoData
+    ): Boolean {
+        return oldItem.id==newItem.id
+    }
+    override fun areContentsTheSame(
+        oldItem: VideoData,
+        newItem: VideoData
+    ): Boolean {
+        return oldItem==newItem
+    }
 }
